978fc0865653a4644241cc013eb7c361f0d7cf48
ilteris <ilteris@gmail.com>
2013-11-13 09:35:54 -0500

    add mainviewcontroller

---
+++ Walker/IKMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  IKMainViewController.h
+//  Walker
+//
+//  Created by ilteris on 11/13/13.
+//  Copyright (c) 2013 ilteris kaplan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKMainViewController : UIViewController
+
+@end
---
+++ Walker/IKMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  IKMainViewController.m
+//  Walker
+//
+//  Created by ilteris on 11/13/13.
+//  Copyright (c) 2013 ilteris kaplan. All rights reserved.
+//
+
+#import "IKMainViewController.h"
+
+@interface IKMainViewController ()
+
+@end
+
+@implementation IKMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
