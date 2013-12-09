76d091213c6dd5e10d7486b0430ddccba8ff5b58
ilteris <ilteris@gmail.com>
2013-12-09 16:03:08 -0500

    add project

---
+++ Walker/IKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IKAppDelegate.h
+//  Walker
+//
+//  Created by ilteris on 12/9/13.
+//  Copyright (c) 2013 ilteris kaplan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Walker/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Walker
+//
+//  Created by ilteris on 12/9/13.
+//  Copyright (c) 2013 ilteris kaplan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IKAppDelegate class]));
+    }
+}
