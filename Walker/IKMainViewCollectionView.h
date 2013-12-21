d86bbe55a78865a15d6c50909a5e4cd5dcd49030
ilteris <ilteris@gmail.com>
2013-12-21 17:51:02 -0500

    add collectionview subclass

---
+++ Walker/IKMainViewCollectionView.h
@@ -0,0 +1,13 @@
+//
+//  IKMainViewCollectionView.h
+//  Walker
+//
+//  Created by ilteris on 12/21/13.
+//  Copyright (c) 2013 ilteris kaplan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IKMainViewCollectionView : UICollectionView
+
+@end
